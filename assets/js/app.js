a28e16554ffb55659afb9781be8bba7aa01e461f
Luis <luis.olguin89@gmail.com>
2020-06-02 23:18:40 -0500

    imported data via D3

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,11 @@
 // @TODO: YOUR CODE HERE!
+
+d3.csv("assets/data/data.csv").then(function(healthData) {
+
+    console.log(healthData);
+
+    var poverty = healthData.map(data => data.poverty);
+    var obesity = healthData.map(data => data.obesity);
+    console.log("Poverty", poverty);
+    console.log("Obesity", obesity);
+});
